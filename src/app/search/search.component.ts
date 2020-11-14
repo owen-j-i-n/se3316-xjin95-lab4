c2722cab3014f908bd214990b22f344e3a960385
owen-j-i-n <71049419+owen-j-i-n@users.noreply.github.com>
2020-11-14 12:41:59 -0500

    Update search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -28,25 +28,6 @@ export class SearchComponent implements OnInit {
     return list; 
   }
 
-  makeSearch(catalog_nbr:string, subject:string, SSR:string){
-   var result = []
-   var div = document.getElementById("resultList")
-   while(div.firstChild){div.removeChild(div.firstChild);}
-    console.log(catalog_nbr,subject,SSR)
 
-    this.taskService.makeSearch(catalog_nbr,subject,SSR).subscribe((finalList: any)=>{//from lab3API
-
-      for(var i=0; i<finalList.length; i++){
-
-        result[i]={
-            catalog_nbr: finalList[i].catalog_nbr,
-            subject: finalList[i].subject,
-            ssr_component: finalList[i].ssr_component
-        }
-        document.getElementById("resultList").appendChild(this.getList(finalList[i].catalog_nbr,finalList[i].subject,finalList[i].ssr_component))
-    }
-    })
-    console.log(result)
-  }
 
 }
